515ebd625b5c9009129c40365f6297d1cdcb502d
sethcwiley <94139645+sethcwiley@users.noreply.github.com>
2021-11-16 12:32:03 -0600

    Add files via upload
    Added the parse function

--- main.py
+++ main.py
@@ -25,6 +25,60 @@ def grabDatabase():
     games = api_instance.get_draft_picks(year=2018)
     removeDlg()
     
+    #Start of Seth's code
+    listy = []
+    import re
+    #puts everything in game in a list and makes each item a string
+    for line in games:
+        line = str(line)
+        thing = re.split(':|\n', line)
+        listy.append(thing)
+    
+    #print(games[0])
+    #print(listy[0])
+    total = {}
+    everything = []
+
+    #puts every item in a dictionary
+    for thing in listy:
+        counter = 0
+        for item in thing:
+            #print(counter)
+            #print(item)
+            if (counter < 9):
+                if (counter < len(thing) - 2) and (counter % 2 == 0):
+                    thing[counter] = thing[counter].replace(",", "")
+                    thing[counter + 1] = thing[counter + 1].replace(",", "")
+                    thing[counter] = thing[counter].replace("'", "")
+                    thing[counter + 1] = thing[counter + 1].replace("'", "")
+                    thing[counter] = thing[counter].replace(" ", "")
+                    thing[counter + 1] = thing[counter + 1].replace(" ", "", 1)
+                    thing[counter] = thing[counter].replace("{", "")
+                    total[thing[counter]] = thing[counter + 1]
+                    #print(total)
+            else:
+                if (counter < len(thing) - 2) and (counter % 2 == 0):
+                    thing[counter + 2] = thing[counter + 2].replace(",", "")
+                    thing[counter + 1] = thing[counter + 1].replace(",", "")
+                    thing[counter + 2] = thing[counter + 2].replace("'", "")
+                    thing[counter + 1] = thing[counter + 1].replace("'", "")
+                    thing[counter + 2] = thing[counter + 2].replace(" ", "")
+                    thing[counter + 1] = thing[counter + 1].replace(" ", "", 1)
+                    thing[counter + 2] = thing[counter + 2].replace("{", "")
+                    thing[counter + 2] = thing[counter + 2].replace("}", "")
+                    total[thing[counter + 1]] = thing[counter + 2]
+                    #print(total)
+            counter += 1
+        #print(total)
+        #print("NEW LINE--------")
+        total_copy = total.copy()
+        everything.append(total_copy)
+        total.clear()
+
+    print(everything[0])
+    #End of Seth's code
+        
+    
 def openMainWindows():
     frm = ttk.Frame(root, padding=10)
     frm.grid()
