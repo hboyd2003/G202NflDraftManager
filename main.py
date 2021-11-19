d84c531658c8300ea0961df9c6a0566091566418
hboyd2003 <harrisonboyd2003@yahoo.com>
2021-11-19 11:54:18 -0600

    Lincense and header

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+#    GM-AID is a tool designed to help NFL teams manage thier draft picks. It is built on Python utilizing Tkinter for GUI.
+#    Copyright (C) 2021  Harrison Boyd, Seth Wiley, Thomas Jackson and Benjamin Hulsey
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+
 from tkinter import *
 import tkinter as tk
 from tkinter.filedialog import askopenfilename
